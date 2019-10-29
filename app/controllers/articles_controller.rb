21c27d3c913d35093ccd7a4f0c3a1164cf8abee8
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-10-29 19:15:08 -0400

    Added create route, show route, and article:reasource

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController 
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was saved to Database"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+end 
