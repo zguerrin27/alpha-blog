3f842e08335f93417fb42bdaa9ccb3a30336daf6
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-11-05 16:47:44 -0500

    Got messages styled

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was saved to Database"
+      flash[:success] = "Article was saved to Database"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   def update
     #@article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else 
       render 'edit'
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
     #@article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
   end 
 
   private
