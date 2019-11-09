c90b887f9349246e40e2a785a07ba27ecb8b4edb
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-11-09 17:51:03 -0500

    Pushing to production

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 3, maximum: 15}
-  validates :description, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :description, presence: true, length: { minimum: 5, maximum: 300 }
 
 end 
