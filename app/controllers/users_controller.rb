c86dd0331fed692359058fb58818876356a50b5c
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-11-18 16:39:58 -0500

    Added form partial for edit and signup users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit 
+    @user = User.find(params[:id])
+
+  end 
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your Account was updated!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
