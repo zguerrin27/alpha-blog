7968dbed99d347e1d531d7c4c1a1cbc07279a1d5
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-12-03 18:04:54 -0500

    Added admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,14 @@
 class User < ActiveRecord::Base
-
-  has_many :articles
-
+  has_many :articles, dependent: :destroy
   before_save {self.email = email.downcase}  
-
   validates :username, presence: true, 
                        uniqueness: { case_sensitive: false}, 
-                       length: { minimum: 3, maximum: 25}
-            
+                       length: { minimum: 3, maximum: 25}        
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false},
                     length: { minimum: 1, maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX }
-
   has_secure_password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_000548) do
+ActiveRecord::Schema.define(version: 2019_12_03_203522) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_000548) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
